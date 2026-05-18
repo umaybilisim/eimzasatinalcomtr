a2e77a6f5b5d6a78fd56b5a39237cbcec49b0b46
AYCANGH <aycanfirtin@hotmail.com>
2026-05-18 23:53:22 +0300

    GitHub Pages deploy workflow eklendi

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
 }
 
 module.exports = nextConfig
